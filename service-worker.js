5eabfa934528ee1d5cac61a05626ea10aa6e29a1
Myko1212 <miko4st@ukr.net>
2026-05-13 08:54:12 -0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,39 +1,54 @@
-const CACHE_NAME = "hay-buy-helper-v2";
-
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./logo.png"
-];
+const CACHE_NAME = "hay-buy-helper-v10";
 
 self.addEventListener("install", event => {
+
   self.skipWaiting();
 
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./logo.png"
+      ]);
+    })
   );
 });
 
 self.addEventListener("activate", event => {
+
   event.waitUntil(
+
     caches.keys().then(cacheNames => {
+
       return Promise.all(
+
         cacheNames.map(cache => {
+
           if (cache !== CACHE_NAME) {
             return caches.delete(cache);
           }
+
         })
+
       );
+
     })
+
   );
 
   self.clients.claim();
+
 });
 
 self.addEventListener("fetch", event => {
+
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+
+    fetch(event.request)
+      .catch(() => caches.match(event.request))
+
   );
+
 });
